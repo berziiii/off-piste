1be90469b92409305490e0dddaf7b721f3c12311
Brian Berzellini <berzelba@gmail.com>
2015-05-03 17:43:15 -0400

    5-1-15
    Edit to some content

--- gruntfile.js
+++ gruntfile.js
@@ -71,7 +71,8 @@ module.exports = function(grunt) {
                         'images/**',
                         '*accounts/**',
                         'js/**',
-                        'css/**'
+                        'css/**',
+                        'Resume.pdf'
                     ],
                     dest: 'dist'
                     }],
